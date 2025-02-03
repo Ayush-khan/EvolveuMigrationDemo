fd30404b147c89646f876276d259b5d48c7505aa
Ayush-Khan <ayushkhana230@gmail.com>
2025-02-03 03:00:01 -0800

    Start demo host of the evolvue

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import "./App.css";
+import Index from "./router/Index";
+
+function App() {
+  // const API_URL = import.meta.env.VITE_API_URL;
+  // const API_URL = import.meta.env.API_URL;
+  // console.log("this is API_URL", API_URL);
+  return (
+    <div className="overflow-x-hidden">
+      <Index />
+    </div>
+  );
+}
+
+export default App;
